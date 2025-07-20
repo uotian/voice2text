e68d883058d010be2b34d383ffcd54689c40cea5
uotian <uotian@gmail.com>
2025-07-20 15:32:44 +0900

    Initial Commit

---
+++ voice2text/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  voice2text
+//
+//  Created by 内藤芳彦 on 2025/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ voice2text/voice2textApp.swift
@@ -0,0 +1,17 @@
+//
+//  voice2textApp.swift
+//  voice2text
+//
+//  Created by 内藤芳彦 on 2025/07/20.
+//
+
+import SwiftUI
+
+@main
+struct voice2textApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
